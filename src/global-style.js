1fc7e2a878d923df0b6a8f735c7ecfc6b4b93551
Alireza Fereydoni <Aliirzw@gmail.com>
2022-01-09 14:53:24 +0330

    Add global styles

---
+++ src/global-style.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html,body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        min-height: 100%;
+        background-color: #f3f8fd;
+        box-sizing:border-box;
+        font-family: san-serif;
+    }
+
+  * {outline: none;}
+
+  a {
+    text-decoration:none;
+    &:hover,
+    &:active,
+    &:visited {
+      text-decoration:none;
+    }
+  }
+    
+`;
+
+const Theme = {
+  colors: {
+    white: "#fff",
+    black: "#000",
+    dark: "#2e384d",
+    light_gray: "#F6F8FC",
+  },
+};
+
+export { GlobalStyle, Theme };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// style
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, Theme } from "./global-style";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

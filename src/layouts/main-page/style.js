3465b0b085cd0720ec0f117d627c24a6de0aa70e
Alireza Fereydoni <Aliirzw@gmail.com>
2022-01-10 20:14:41 +0330

    Optimize of layout style

--- src/layouts/main-page/style.js
+++ src/layouts/main-page/style.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const MainLayoutWrapper = styled.div`
   width: 100%;
   height: 100%;
-  min-height: 100vh;
 
   .Container {
     padding: 1rem min(4%, 45px);
